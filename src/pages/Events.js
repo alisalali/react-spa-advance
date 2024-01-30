170981c853d812b28a482b8d640c404d2d0b1f93
alisalali <alis.alali@hotmail.com>
2024-01-30 23:30:16 +0300

    more loader usage

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,10 +1,8 @@
-import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 const EventsPage = () => {
-  const events = useLoaderData();
   return (
     <>
-      <EventsList events={events} />
+      <EventsList />
     </>
   );
 };
