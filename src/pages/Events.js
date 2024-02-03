8bd4f3b46ac59c99944b04ea59cf7447898ead47
alisalali <alis.alali@hotmail.com>
2024-02-03 22:13:11 +0300

    one way to handel error loader router

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -3,6 +3,9 @@ import EventsList from "../components/EventsList";
 const EventsPage = () => {
   const data = useLoaderData();
   const events = data.events;
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
   return (
     <>
       <EventsList events={events} />
@@ -13,9 +16,10 @@ const EventsPage = () => {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/eventsxx");
 
   if (!response.ok) {
+    return { isError: true, message: "Could not fetch events" };
   } else {
     return response;
   }
