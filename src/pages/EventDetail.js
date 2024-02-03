3ef2bed6a85b6a1b87cea9034d699155c933a746
alisalali <alis.alali@hotmail.com>
2024-02-04 01:24:54 +0300

    use dynamic route and loader()

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -1,8 +1,23 @@
-import { useParams } from "react-router-dom";
+import { json, useLoaderData, useParams } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
-  const params = useParams();
-  return <div>EventDetailPage {params.eventId}</div>;
+  const data = useLoaderData();
+  const event = data.event;
+
+  return <EventItem event={event} />;
 };
 
 export default EventDetailPage;
+
+export const loader = async ({ request, params }) => {
+  const id = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw json({ message: "Error fetching event" }, { status: 500 });
+  } else {
+    console.log(response);
+    return response;
+  }
+};
