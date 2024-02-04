d332dd133786990cce311da2f9cc291d5c1c44c0
alisalali <alis.alali@hotmail.com>
2024-02-04 14:12:54 +0300

    submitting Form with Action() route , extracting data using request.formData().data.get

--- src/pages/NewEvent.js
+++ src/pages/NewEvent.js
@@ -1,5 +1,35 @@
+import { json, redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
 const NewEventPage = () => {
-  return <div>NewEventPage</div>;
+  return <EventForm />;
 };
 
 export default NewEventPage;
+export const action = async ({ request, params }) => {
+  const data = await request.formData();
+  console.log(data);
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  console.log(eventData);
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event." }, { status: 500 });
+  }
+
+  return redirect("/events");
+};
