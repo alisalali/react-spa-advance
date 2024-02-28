949bc28f693474e45615cc725433f4284558262b
alisalali <alis.alali@hotmail.com>
2024-02-28 12:15:52 +0300

    reusing action through request method

--- src/App.js
+++ src/App.js
@@ -27,11 +27,12 @@ import EventDetailPage, {
   loader as eventDetailLoader,
   action as eventDeleteAction,
 } from "./pages/EventDetail";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
+import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventRootPage from "./pages/EventRoot";
 import ErrorPage from "./pages/Error";
+import { action as manipulateEventAction } from "./components/EventForm";
 
 function App() {
   const router = createBrowserRouter([
@@ -60,10 +61,18 @@ function App() {
                   element: <EventDetailPage />,
                   action: eventDeleteAction,
                 },
-                { path: "edit", element: <EditEventPage /> },
+                {
+                  path: "edit",
+                  element: <EditEventPage />,
+                  action: manipulateEventAction,
+                },
               ],
             },
-            { path: "new", element: <NewEventPage />, action: newEventAction },
+            {
+              path: "new",
+              element: <NewEventPage />,
+              action: manipulateEventAction,
+            },
           ],
         },
       ],
